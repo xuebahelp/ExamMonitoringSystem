205a6da0b7de4a0d2ca6f8e841749728650bec43
easinlee <1375343893@qq.com>
2025-04-17 19:19:57 +0800

    init

---
+++ datasets/ExamMonitoring/label_name.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+@Auth ： 逗逗班学Python
+博客园、知乎：逗逗班学Python
+Bilibili：逗逗班学Python
+面包多: 逗逗班学Python
+代码地址见以下博客中给出:
+https://space.bilibili.com/456667721
+https://www.cnblogs.com/deeppython
+
+@IDE ：PyCharm
+
+    
+    
+    
+    
+    
+    
+    
+    
+
+"""
+Chinese_name = {"cheating": "疑似作弊", "good": "良好", "normal": "正常"}
+Label_list = list(Chinese_name.values())
